a4b22401579aceade9044249b4aec43b610efa69
14Rahul <48749887+14Rahul@users.noreply.github.com>
2024-02-25 18:31:36 +0530

    Update cicdv2.sh

--- cicdv2.sh
+++ cicdv2.sh
@@ -83,8 +83,9 @@ build_and_push_image() {
   
   cd "$REPO_HOME/repo/" || log_and_exit "Failed to change to repo directory"
   echo "Logging in to AWS ECR"
-  echo 'aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "532968567499.dkr.ecr.$REGION.amazonaws.com"'
-  aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "532968567499.dkr.ecr.$REGION.amazonaws.com"
+  #echo 'aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "532968567499.dkr.ecr.$REGION.amazonaws.com"'
+  #aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "532968567499.dkr.ecr.$REGION.amazonaws.com"
+  aws ecr get-login-password --region "ap-south-1" | docker login --username AWS--password-stdin "532968567499.dkr.ecr.ap-south-1.amazonaws.com"
   aws codeartifact login --tool pip --repository propy --domain prodigaltech --domain-owner 532968567499 --region ap-south-1
 #  echo "Pulling the latest image from ECR..."
 #  docker pull "$ecr_repo_uri:$IMAGE_TAG" || log_and_exit "Failed to pull image"
